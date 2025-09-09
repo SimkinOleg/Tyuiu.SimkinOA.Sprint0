38bbba685608b4dabe8fbda9bd0eb36c5fcdc953
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-09-09 15:04:30 +0500

    Task2

---
+++ Tyuiu.SimkinOA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SimkinOA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.SimkinOA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Олег";
+            var res = DataService.GetMessage(name);
+
+
+            Assert.AreEqual("Привет..., Олег", res);
+        }
+    }
+}
---
+++ Tyuiu.SimkinOA.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SimkinOA.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SimkinOA.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef35a015-88ba-4670-81f6-1b6eea58c8c8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
