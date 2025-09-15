e5d5e7d404b78c96e4e3e8012bb098d2f73a051d
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-09-15 20:41:28 +0500

    Task6

---
+++ Tyuiu.SimkinOA.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SimkinOA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SimkinOA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bb18b936-871d-4142-a44e-e7c8cad6bc6a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
